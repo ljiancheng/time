547d93c5726f2df9658eb9e3037b819f659ac8c5
Administrator <Administrator@ts70722>
2019-03-27 11:53:50 +0800

    项目前期

---
+++ ROOM/ROOM/Heres2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROOM
+{
+    public class Heres2
+    {
+        //生成一年级题目
+    }
+}
--- ROOM/ROOM/Program.cs
+++ ROOM/ROOM/Program.cs
@@ -10,6 +10,9 @@ namespace ROOM
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("请输入所选年级,1为一年级、2为二年级、3为三年级、4为四年级、5为五年级、6为六年级");
+            int quantity = int.Parse(Console.ReadLine());
+            Create.A(quantity);
         }
     }
 }
