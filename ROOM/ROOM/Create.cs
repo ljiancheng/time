7035f59fe9ed662a92d3caf2bdc22231becaf64b
Administrator <Administrator@ts70722>
2019-03-28 12:10:24 +0800

    项目中期

--- ROOM/ROOM/Create.cs
+++ ROOM/ROOM/Create.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Collections;
+using System.IO;
 
 namespace ROOM
 {
@@ -11,7 +14,11 @@ namespace ROOM
         //生成题目
         public static void A(int quantity)
         {
-
+            Hashtable fourOperations = new Hashtable();
+            Console.WriteLine("正在生成题目,请稍等···");
+            switch (quantity)
+            {
+            }
         }
     }
 }
--- ROOM/ROOM/Heres2.cs
+++ ROOM/ROOM/Heres2.cs
@@ -9,5 +9,46 @@ namespace ROOM
     public class Heres2
     {
         //生成一年级题目
+        public static string topicone(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
+        //生成二年级题目
+        public static string topictwo(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
+        //生成三年级题目
+        public static string topicthree(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
+        //生成四年级题目
+        public static string topicfour(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
+        //生成五年级题目
+        public static string topicfive(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
+        //生成六年级题目
+        public static string topicsix(int quantity)
+        {
+            string ret;
+            ret = Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity) + " " + Heres1.operators() + " " + Heres1.integer(quantity);
+            return ret;
+        }
     }
 }
